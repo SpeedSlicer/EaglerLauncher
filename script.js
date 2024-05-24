9c9f99f2840948b9fd0e649a63aef099b8fd97e0
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2024-05-24 19:52:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
         content.innerHTML = `
 
                              <ul>
-                                <li>EaglercraftX: Regular release (current version: U30).</li>
+                                <li>EaglercraftX: Regular release (current version: U31).</li>
                                 <li>EaglerForge: Mod Support for Eagler</li>
                                 <li>EaglerL 1.9 Beta: Stable Eagler 1.9, fewer features, but ensured to work.</li>
                                 <li>EaglerL 1.9 Unstable: Unstable Eagler 1.9</li>
